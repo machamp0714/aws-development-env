0bacbcf013630bef49f044dd72d8b0f17f0a7291
machamp0714 <rateliasudaisuki@gmail.com>
2021-03-14 22:45:10 +0900

    add: public subnet

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,5 +1,12 @@
 resource "aws_vpc" "vpc" {
-  cidr_block          = "10.0.0.0/16"
-  enable_dns_support  = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
 }
